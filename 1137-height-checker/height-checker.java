5b11fcc9f47089c946c6b42cfa79185edf3c99b2
krituSu <kritagyamaurya6@gmail.com>
2026-07-04 13:17:05 +0530

    Time: 2 ms (90.98%) | Memory: 43.2 MB (86.03%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -27,5 +27,29 @@ class Solution {
         }
         
         return count;
+
+        //Not my Approach but leraned something that we can solve
+        //without sorting too
+        // int[] heightToFreq = new int[101];
+        
+        // for (int height : heights) {
+        //     heightToFreq[height]++;
+        // }
+        
+        // int result = 0;
+        // int curHeight = 0;
+        
+        // for (int i = 0; i < heights.length; i++) {
+        //     while (heightToFreq[curHeight] == 0) {
+        //         curHeight++;
+        //     }
+            
+        //     if (curHeight != heights[i]) {
+        //         result++;
+        //     }
+        //     heightToFreq[curHeight]--;
+        // }
+        
+        // return result;
     }
 }
