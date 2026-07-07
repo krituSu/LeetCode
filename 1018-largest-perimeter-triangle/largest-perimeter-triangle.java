e16e0845d34794975c076e0d77a3b4d1244cdf77
krituSu <kritagyamaurya6@gmail.com>
2026-07-07 14:19:19 +0530

    Time: 15 ms (12.21%) | Memory: 47.7 MB (57.90%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
@@ -1,39 +1,26 @@
 class Solution {
     public int largestPerimeter(int[] nums) {
         int n = nums.length;
-        //Sorting
-         for(int i = 1; i < n; i++){
-            int prev = i - 1;
-            int curr = i;
-            int currValue = nums[i];
-            while( currValue < nums[prev] && prev >= 0){
-                nums[prev + 1] = nums[prev];
-                prev--;
-                if(prev < 0){
-                    break;
-                }
-            }
-            nums[prev + 1] = currValue;
-        }
-        //Getting the perimeter
+        Arrays.sort(nums);
+
         int maxPerimeter = Integer.MIN_VALUE;
         int perimeter = 0;
-        for(int i = 0; i < n - 2; i++){
+        int i = 0;
+        int j = i + 2;
+        while(j < n){
             int val = nums[i] + nums[i + 1];
-            for(int j = i + 2; j < n; j++){
-                if(val > nums[j]){
-                    perimeter = val + nums[j];
-                    if(perimeter > maxPerimeter){
-                        maxPerimeter = perimeter;
-                    }
-                }
+            if(val > nums[j]){
+                perimeter = val + nums[j];
             }
+            if(perimeter > maxPerimeter){
+                maxPerimeter = perimeter;
+            }            
+            if(j == n - 1 && maxPerimeter < 0){
+                return 0;
+            }
+            i++;
+            j++;
         }
-        if(maxPerimeter > 0){
-            return maxPerimeter;
-        }
-        else{
-            return 0;
-        }
+        return maxPerimeter;
     }
 }
