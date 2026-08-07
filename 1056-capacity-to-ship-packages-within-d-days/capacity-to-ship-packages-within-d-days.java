d413258808e537b6ef3e0e625864b1c603306dee
krituSu <kritagyamaurya6@gmail.com>
2026-08-07 16:56:03 +0530

    Time: 9 ms (98.84%) | Memory: 50.4 MB (20.39%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,40 @@
+class Solution {
+    static boolean isValidWeight(int[] weights, int days, int maxWeight){
+        int day = 1;
+        int weight = 0;
+        for(int i = 0; i < weights.length; i++){
+           if(weight + weights[i] > maxWeight){
+            day++;
+            weight = 0;
+            if(day > days || weights[i] > maxWeight){
+                return false;
+            }
+           }
+           weight += weights[i];
+        }
+        return true;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+         int sum = 0;
+        for(int i = 0; i < weights.length;i++){
+            sum += weights[i];
+        }
+
+        int start = 0;
+        int end = sum;
+        int ans = 0;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(isValidWeight(weights, days, mid)){
+                ans = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
